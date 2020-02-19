1b6a28ee919f7f3054386cc92e723d70f355f051
Ezazhel <Ezazhel@gmail.com>
2020-02-19 16:36:18 +0100

    Ajout de la suppresion d'une track dans la liste

--- src/components/createPlaylist/CreatePlaylist.js
+++ src/components/createPlaylist/CreatePlaylist.js
@@ -3,72 +3,77 @@ import CreatePlaylistTable from "./CreatePlaylistTable.js";
 import InputPlaylist from "./InputPlaylist.js";
 
 const CreatePlaylist = () => {
-  const [name, setName] = useState("");
+    const [name, setName] = useState("");
 
-  const [responses, setResponses] = useState([]);
+    const [responses, setResponses] = useState([]);
+    const removeTrack = id => {
+        setResponses(responses.filter(item => item.id !== id));
+    };
+    const onNameChange = event => setName(event.target.value);
 
-  const onNameChange = event => setName(event.target.value);
+    const onImportchange = event => {
+        if (event.target.value === "") return;
 
-  const onImportchange = event => {
-    if (event.target.value === "") return;
-
-    // Axios.post(CONSTANT.URLPLAYLIST, event.target.value).then()
-    setResponses([
-      {
-        id: 1,
-        title: "Belle",
-        artist: "Les dix commandements",
-        track: {
-          yttitle: "Belle - Les dix commandements"
-        }
-      },
-      {
-        id: 2,
-        title: "Je t'aime",
-        artist: "Céline dion",
-        track: {
-          yttitle: "Je t'aime - Céline dion"
-        }
-      },
-      {
-        id: 3,
-        title: "Yaka",
-        artist: "Shakira",
-        track: {
-          yttitle: "Yaka - Shakira"
-        }
-      },
-      {
-        id: 4,
-        title: "She wolf",
-        artist: "Shakira",
-        track: {
-          yttitle: "She wolf - Shakira"
-        }
-      }
-    ]);
-  };
-  return (
-    <div>
-      <form action="">
-        <fieldset>
-          <InputPlaylist
-            value={name}
-            name="PlayListName"
-            onChange={onNameChange}
-            placeholder="nom de la playlist"
-          />
-          <InputPlaylist
-            value={name}
-            name="UrlPlaylist"
-            onChange={onImportchange}
-            placeholder="import playlist"
-          />
-          <CreatePlaylistTable responses={responses} />
-        </fieldset>
-      </form>
-    </div>
-  );
+        // Axios.post(CONSTANT.URLPLAYLIST, event.target.value).then()
+        setResponses([
+            {
+                id: 1,
+                title: "Belle",
+                artist: "Les dix commandements",
+                track: {
+                    yttitle: "Belle - Les dix commandements"
+                }
+            },
+            {
+                id: 2,
+                title: "Je t'aime",
+                artist: "Céline dion",
+                track: {
+                    yttitle: "Je t'aime - Céline dion"
+                }
+            },
+            {
+                id: 3,
+                title: "Yaka",
+                artist: "Shakira",
+                track: {
+                    yttitle: "Yaka - Shakira"
+                }
+            },
+            {
+                id: 4,
+                title: "She wolf",
+                artist: "Shakira",
+                track: {
+                    yttitle: "She wolf - Shakira"
+                }
+            }
+        ]);
+    };
+    return (
+        <div>
+            <form action="">
+                <fieldset>
+                    <InputPlaylist
+                        value={name}
+                        name="PlayListName"
+                        onChange={onNameChange}
+                        placeholder="nom de la playlist"
+                    />
+                    <InputPlaylist
+                        value={name}
+                        name="UrlPlaylist"
+                        onChange={onImportchange}
+                        placeholder="import playlist"
+                    />
+                    <CreatePlaylistTable
+                        responses={responses}
+                        removeTrack={removeTrack}
+                    />
+                </fieldset>
+            </form>
+        </div>
+    );
 };
 
 export default CreatePlaylist;
