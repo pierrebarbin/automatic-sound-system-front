ed434d5305d0e483f2624dd846f1981bc0f405a2
Ezazhel <Ezazhel@gmail.com>
2020-02-20 16:23:28 +0100

    Composant profile avec tabs playlis, historique, follow

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,14 +1,18 @@
 import Index from "../components/pages/Index.js";
 import Login from "../components/pages/auth/Login.js";
-
+import Profile from "../components/pages/profile/Profile.js";
 export const routes = [
     {
         path: "/",
         component: Index,
-        exact: true,
+        exact: true
     },
     {
         path: "/login",
         component: Login
+    },
+    {
+        path: "/profile",
+        component: Profile
     }
 ];
