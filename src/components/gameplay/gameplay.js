6a27d527aa51137e3c0d7ac43e8e5f8a50da76ac
NHerve <herve.04@hotmail.fr>
2020-02-20 14:48:17 +0100

    add input and form for levenstein

--- src/components/gameplay/gameplay.js
+++ src/components/gameplay/gameplay.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import PropTypes from 'prop-types'
-// import GameSearch from //unTruc qui arrivera plus tard
+// import GameSearch from './GameSearch'
 // import YtPlayer from //unTruc qui arrivera plus tard
 
 
@@ -13,30 +13,39 @@ const Gameplay = props => {
         id: 1,
         url: "https://www.youtube.com/watch?v=y6120QOlsfU",
         title: "Sandstorm",
-        singer: "Darude"
+        singer: "Darude",
+        bArtiste: true,
+        bTitle: true
     },
     {
         id: 2,
         url: "https://www.youtube.com/watch?v=zA52uNzx7Y4",
         title: "Blue",
-        singer: "Eiffel 65"
+        singer: "Eiffel 65",
+        bArtiste: true,
+        bTitle: true
     },
     {
         id: 3,
         url: "https://www.youtube.com/watch?v=SYnVYJDxu2Q",
         title: "Rasputin",
-        singer: "Boney M."
+        singer: "Boney M.",
+        bArtiste: true,
+        bTitle: true
     },
     {
         id: 4,
         url: "https://www.youtube.com/watch?v=Nnu1E5Kslig",
         title: "Stairway To Heaven",
-        singer: "Led Zeppelin"
+        singer: "Led Zeppelin",
+        bArtiste: true,
+        bTitle: true
     }
   ]);
 
-  const [seconds, setSeconds] = useState(30);
+  const [seconds, setSeconds] = useState(20);
   const [isActive, setIsActive] = useState(false);
+  const [valueInput, setValueInput] = useState("");
 
   useEffect(() => {
     let interval = null;
@@ -62,6 +71,10 @@ const Gameplay = props => {
     setIsActive(!isActive);
   }
 
+  const handleSubmit = (event) => {
+    alert(`call le levenstein avec  ${valueInput} et les reponses`)
+}
+
   return (
     <div className="app">
       <div className="time">
@@ -69,8 +82,20 @@ const Gameplay = props => {
       </div>
       <div>
         <button onClick={toggle}>
-        {isActive ? '' : 'Start'}
-      </button></div>
+          {isActive ? '' : 'Start'}
+        </button>
+      </div>
+      <br/>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={valueInput}
+            onChange={e => setValueInput(e.target.value)}
+          />
+          <input type="submit" value="submit"/>
+        </form>
+      </div>
     </div>
   );
 };
