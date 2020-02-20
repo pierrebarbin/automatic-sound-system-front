f4c8e9e556f252dadebd3738ab1ef3d4d2a3448c
NHerve <herve.04@hotmail.fr>
2020-02-20 11:10:39 +0100

    ajout d'un timer fonctionnel

--- src/components/gameplay/gameplay.js
+++ src/components/gameplay/gameplay.js
@@ -1,74 +1,79 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import PropTypes from 'prop-types'
 // import GameSearch from //unTruc qui arrivera plus tard
 // import YtPlayer from //unTruc qui arrivera plus tard
 
 
-// TODO faire fonctionner le timer
+// voir https://upmostly.com/tutorials/build-a-react-timer-component-using-hooks
 
 const Gameplay = props => {
-    const [date,setDate] = useState({date: new Date()})
-    const [musique, setMusique] = useState(
-        [
-        {
-            id: 1,
-            url: "https://www.youtube.com/watch?v=y6120QOlsfU",
-            title: "Sandstorm",
-            singer: "Darude"
-        },
-        {
-            id: 2,
-            url: "https://www.youtube.com/watch?v=zA52uNzx7Y4",
-            title: "Blue",
-            singer: "Eiffel 65"
-        },
-        {
-            id: 3,
-            url: "https://www.youtube.com/watch?v=SYnVYJDxu2Q",
-            title: "Rasputin",
-            singer: "Boney M."
-        },
-        {
-            id: 4,
-            url: "https://www.youtube.com/watch?v=Nnu1E5Kslig",
-            title: "Stairway To Heaven",
-            singer: "Led Zeppelin"
-        }
-    ]);
+  const [musique, setMusique] = useState(
+  [
+    {
+        id: 1,
+        url: "https://www.youtube.com/watch?v=y6120QOlsfU",
+        title: "Sandstorm",
+        singer: "Darude"
+    },
+    {
+        id: 2,
+        url: "https://www.youtube.com/watch?v=zA52uNzx7Y4",
+        title: "Blue",
+        singer: "Eiffel 65"
+    },
+    {
+        id: 3,
+        url: "https://www.youtube.com/watch?v=SYnVYJDxu2Q",
+        title: "Rasputin",
+        singer: "Boney M."
+    },
+    {
+        id: 4,
+        url: "https://www.youtube.com/watch?v=Nnu1E5Kslig",
+        title: "Stairway To Heaven",
+        singer: "Led Zeppelin"
+    }
+  ]);
 
-    componentDidMount => {
-        this.timerID = setInterval(
-          () => this.tick(),
-          1000
-        );
-    };
+  const [seconds, setSeconds] = useState(30);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
     
-    tick =>{
-        this.setDate({
-            date: new Date()
-        });
-    };
+    if (isActive) {
+      if(seconds != 0){
+        interval = setInterval(() => {
+          setSeconds(seconds => seconds - 1);
+        }, 1000);
+      }else{
+        console.log("stop")
+        setSeconds(setSeconds => 30);
+      }
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
 
-    return (
-        <div>
+    return () => clearInterval(interval);
 
-            <h1>Hello, world!</h1>
-            <h2>It is  {this.date.date.toLocaleTimeString()}.</h2>
+  }, [isActive, seconds]);
 
+  function toggle() {
+    setIsActive(!isActive);
+  }
 
-
-
-
-            <div>
-                {/* <YtPlayer messages={messages}/> */}
-            </div>
-            <div>
-                {/* <GameSearch addMessages={addMessage}/> */}
-            </div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div>
+        <button onClick={toggle}>
+        {isActive ? '' : 'Start'}
+      </button></div>
+    </div>
+  );
+};
 
 
 export default Gameplay
