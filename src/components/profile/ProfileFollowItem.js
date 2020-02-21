942c7b26e2c0f03be54da97694bbcaf7f037b92d
Pierre B <pierre.barbin59@gmail.com>
2020-02-21 14:00:58 +0100

    Design profile page

--- src/components/profile/ProfileFollowItem.js
+++ src/components/profile/ProfileFollowItem.js
@@ -3,9 +3,9 @@ import React from "react";
 const ProfileFollowItem = ({ componentsParameters }) => {
     const { creator, date } = componentsParameters;
     return (
-        <div>
-            <label>{creator}</label>
-            <label>{date}</label>
+        <div className="flex flex-row">
+            <label className="flex-1">{creator}</label>
+            <label className="flex-1">{date}</label>
         </div>
     );
 };
