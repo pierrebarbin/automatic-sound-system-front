1c16821321e18fb04634feda992aa06876f9cefa
Kérian <kerianmontes@gmail.com>
2020-03-05 16:14:37 +0100

    home wording

--- src/components/table/playlist/play/PlayPlaylistTable.js
+++ src/components/table/playlist/play/PlayPlaylistTable.js
@@ -13,6 +13,8 @@ const PlayPlaylistTable = ({playlists}) => {
     return (
         <div className="bg-gray-900 text-gray-500 p-4 rounded-lg shadow-lg">
             <div className="pt-2">
+                <h1 className="text-xl font-semibold">Jouer à une partie</h1>
+                <div className="border-b border-gray-900 pt-1"/>
                 <div>{playPlaylistTableItems}</div>
             </div>
         </div>
